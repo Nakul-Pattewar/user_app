cc69a0c9bd7ab68054e93e4ce272c361d94002d3
Nakul-Pattewar <nakul.pattewar@kylas.io>
2025-02-12 15:35:46 +0530

    refactor code as per commens

--- lib/common/constants/strings.dart
+++ lib/common/constants/strings.dart
@@ -1,11 +1,10 @@
 const String listPageTitle = 'Users list';
-const String errorPageTitle = 'Users list';
+const String errorPageTitle = 'Error';
 const String usersListErrorMessage = 'Error occurred while loading data...';
 const String noUsersTextMessage = 'No users found';
 const String usersListFilePath = 'assets/users_list.json';
 const String gorestBaseUrl = 'https://gorest.co.in';
-const String usersListEndpoint = '/public/v2/users';
-const String editUserEndpoint = '/public/v2/users';
+const String userApiEndpoint = '/public/v2/users';
 const String clientErrorMessage = 'Client Error';
 const String serverErrorMessage = 'Server Error';
 const String unexpectedErrorMessage = 'Unexpected Error';
--- lib/features/user/edit/bloc/edit_user_cubit.dart
+++ lib/features/user/edit/bloc/edit_user_cubit.dart
@@ -3,8 +3,9 @@ import 'dart:async';
 import 'package:user_app/common/constants/strings.dart';
 import 'package:user_app/common/state/ui_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:user_app/features/user/edit/network/edit_user_api.dart';
 import 'package:user_app/features/user/list/network/user_response.dart';
+
+import '../../list/network/user_api.dart';
 
 class EditUserCubit extends Cubit<UiState<bool>> {
   EditUserCubit() : super(Default());
@@ -12,7 +13,7 @@ class EditUserCubit extends Cubit<UiState<bool>> {
   FutureOr<void> updateUser(UserResponse user) async {
     emit(Loading());
     try {
-      final apiResponse = await EditUserApi().updateUser(user);
+      final apiResponse = await UserApi().updateUser(user);
       if (apiResponse) {
         emit(Success<bool>(apiResponse));
       } else {
